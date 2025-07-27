a9f502231881dbca080bc1b29a66bc2d824498c2
manish <manish.guturu@hiringhood.com>
2025-07-27 23:18:09 +0530

    Final change

--- movie-explorer/src/App.tsx
+++ movie-explorer/src/App.tsx
@@ -24,9 +24,9 @@ function App() {
         <Route
           path="/explorer"
           element={
-            <RequireAuth>
+            
               <Explorer />
-            </RequireAuth>
+            
           }
         />
         <Route
